8da6f549163a34c18c9bdca10950a5f1ef873e87
RC <rckashyapcpe@gmail.com>
2023-06-13 14:12:15 +0800

    made minor UI changes

--- app/src/main/java/com/example/yesorno/MainActivity.kt
+++ app/src/main/java/com/example/yesorno/MainActivity.kt
@@ -7,6 +7,7 @@ import com.example.yesorno.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding.elevatedButtonDecide.setOnClickListener { getYesOrNo() }
 
     }
+
     private fun getYesOrNo() {
         showToast()
 
@@ -23,19 +25,19 @@ class MainActivity : AppCompatActivity() {
         val decisionStyle = (1..3).random()
         if (decision == 1) {
             if (decisionStyle == 1) {
-                binding.textViewDecision.text = "YES"
+                binding.textViewDecision.text = getString(R.string.text_yes)
             } else if (decisionStyle == 2) {
-                binding.textViewDecision.text = "YES!"
+                binding.textViewDecision.text = getString(R.string.text_yes1)
             } else {
-                binding.textViewDecision.text = "YES."
+                binding.textViewDecision.text = getString(R.string.text_yes2)
             }
         } else {
             if (decisionStyle == 1) {
-                binding.textViewDecision.text = "NO"
+                binding.textViewDecision.text = getString(R.string.text_no)
             } else if (decisionStyle == 2) {
-                binding.textViewDecision.text = "NO!"
+                binding.textViewDecision.text = getString(R.string.text_no1)
             } else {
-                binding.textViewDecision.text = "NO."
+                binding.textViewDecision.text = getString(R.string.text_no2)
             }
         }
     }
